022f2b83dea1c427d7eafe2cf44cc716d953c3ab
Matthew Verbryke <verbrymr@mail.uc.edu>
2021-06-03 16:28:02 -0400

    Split power supply and magnetometer interfaces into separate classes.

--- helmholtz_cage/instruments.py
+++ helmholtz_cage/instruments.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
 """
-Script to connect with power supplies and magnetometer.
+Objects and functions to connect with power supplies and magnetometers.
 
-Copyright 2018-2019 UC CubeCats
+Copyright 2018-2021 UC CubeCats
 All rights reserved. See LICENSE file at:
 https://github.com/uccubecats/Helmholtz-Cage/LICENSE
 Additional copyright may be held by others, as reflected in the commit history.
 
 Originally written by Jason Roll (rolljn@mail.uc.edu)
 
-*** denotes unfinished section
-TODO: this may be broken, need to run through it and check.
+TODO: run through it and check new classes/functions.
 """
 
 
@@ -20,18 +19,16 @@ import re
 import serial
 from threading import Thread
 import time
-import visa
+import pyvisa as visa
 
 import numpy as np
 
 
 logger = logging.getLogger(__name__)
-
 # visa.log_to_screen()
-rm = visa.ResourceManager()
 
 
-class ReadLine:
+class ReadLine(object):
     """
     A pyserial object wrapper for reading line.
     
@@ -59,16 +56,19 @@ class ReadLine:
             else:
                 self.buf.extend(data)
 
-
-class Instruments:
-
+class GPIBDaisyChainPS(object):
+    """
+    An object for dealing with a set of three GPIB-capable power supplies 
+    connected using a GPIB daisy chain.
+    """
+    
     def __init__(self):
         
-        # Note: Check these are correct for you
+        # Note: Check these are correct for your setup
         self.x_id = "GPIB0::3::INSTR"
         self.y_id = "GPIB0::5::INSTR"
         self.z_id = "GPIB0::4::INSTR"
-        self.x, self.y, self.z, self.mag = 4 * ["No connection"]
+        self.x, self.y, self.z = 3 * ["No connection"]
         self.log_data = "ON"
         self.connections_checked = True
 
@@ -119,18 +119,6 @@ class Instruments:
                             self.z, err))
                     self.z = "No connection"
 
-        # Connect the magnetometer
-        try:
-            self.mag = serial.Serial('COM5', baudrate=9600, timeout=0.1,
-                                     xonxoff=0, rtscts=0,
-                                     interCharTimeout=None)
-                                     
-            # Test that data can be read from connection
-            field_string = self.mag.readline().decode("utf-8")
-        except Exception as err:
-            print("No connection established to magnetometer | {}".format(err))
-            self.mag = "No connection"
-
     def send_voltage(self, x_voltage, y_voltage, z_voltage):
         """
         Send the commanded voltage values to the power supplies.
@@ -171,83 +159,6 @@ class Instruments:
         
         pass
         # print("field stopped)
-
-    def get_magnetometer_field(self):
-        """
-        Read the current field values from the magnetometer.
-        
-        NOTE: Buffer reset is needed to properly get the current field 
-              values. In the input buffer is all the lines of data written 
-              out from magnetometer, which grows very quickly. This leads
-              to a backlog of data, which gets read out much slower
-              than real time (so old data is being displayed).
-              
-        NOTE: "time.sleep" allows the input buffer to populate with 
-              multiple lines, since the reset could happen in the middle
-              of a line. This way a complete set of field values should
-              be read.
-              
-        NOTE: The magnetometer input buffer cannot be reset if it's not
-              connected.
-        """
-
-        valid_characters = ["0", "1", "2", "3", "4", "5",
-                            "6", "7", "8", "9", ".", "-"]
-
-        try:
-            
-            # Reset serial buffer to pervent it from growing too large.
-            self.mag.reset_input_buffer()
-            time.sleep(0.001)
-            field_string = self.mag.readline().decode("utf-8")
-            print("reading {} from magnetometer".format(field_string))
-            x_string, y_string, z_string = "", "", ""
-            currently_reading = None
-
-            with open("temp_mag_field_raw.txt", "a") as f:
-                f.write(field_string + "\n")
-                print("read in field data")
-
-            # If currently reading for float, add character if it's valid
-            for character in field_string:
-                if (currently_reading == "X") and (character in valid_characters):
-                    x_string += character
-                if (currently_reading == "Y") and (character in valid_characters):
-                    y_string += character
-                if (currently_reading == "Z") and (character in valid_characters):
-                    z_string += character
-
-                # See if it's time to change what is being read
-                if character == "X":
-                    currently_reading = "X"
-                elif character == "Y":
-                    currently_reading = "Y"
-                elif character == "Z":
-                    currently_reading = "Z"
-
-            # Convert the string chunks to floats
-            try:
-                x_field = float(x_string)
-            except Exception as err:
-                x_field = None
-                print("Could not determine x field from string {} | {}"
-                      .format(x_string, err))
-            try:
-                y_field = float(y_string)
-            except Exception as err:
-                y_field = None
-                print("Could not determine y field from string {} | {}"
-                      .format(y_string, err))
-            try:
-                z_field = float(z_string)
-            except Exception as err:
-                z_field = None
-                print("Could not determine z field from string {} | {}"
-                      .format(z_string, err))
-
-            return x_field, y_field, z_field
-        except AttributeError:
-            return 999, 999, 999  # so that it is not confused with 0.0
 
     def get_requested_voltage(self):
         """
@@ -309,6 +220,110 @@ class Instruments:
         return (x_out, y_out, z_out)
 
 
+class SerialMagnetometer(object):
+    """
+    An object for magnetometers using a serial interface.
+    """
+    
+    def __init__(self, address):
+        
+        # Initialize state variables
+        self.address = address
+        self.connected = False
+        
+    def connect_to_device(self):
+        """
+        Attempt to connect to the magnetometer using Serial.
+        
+        TODO: Test
+        """
+        
+        try:
+            self.interface = serial.Serial(self.address, baudrate=9600,
+                                           timeout=0.1, xonxoff=0, 
+                                           rtscts=0, interCharTimeout=None)
+            
+            # Test that data can be read from connection
+            field_string = self.mag.readline().decode("utf-8")
+            self.connected = True
+        except Exception as err:
+            print("No connection established to magnetometer | {}".format(err))
+            self.connected = False
+            
+        return self.connected
+        
+    def get_field_strength(self):
+        """
+        Read the current magnetic field values from the magnetometer.
+        
+        TODO: Test
+        """
+       
+        valid_characters = ["0", "1", "2", "3", "4", "5",
+                            "6", "7", "8", "9", ".", "-"]
+
+        try:
+            
+            # Reset serial buffer to pervent it from growing too large.
+            self.interface.reset_input_buffer()
+            time.sleep(0.001)
+            
+            # Read in data
+            field_string = self.interface.readline().decode("utf-8")
+            print("reading {} from magnetometer".format(field_string))
+            x_string, y_string, z_string = "", "", ""
+            currently_reading = None
+            
+            # Record raw output
+            with open("temp_mag_field_raw.txt", "a") as f:
+                f.write(field_string + "\n")
+                print("read in field data")
+
+            # If currently reading for float, add character if it's valid
+            for character in field_string:
+                if (currently_reading == "X") and (character in valid_characters):
+                    x_string += character
+                if (currently_reading == "Y") and (character in valid_characters):
+                    y_string += character
+                if (currently_reading == "Z") and (character in valid_characters):
+                    z_string += character
+
+                # See if it's time to change what is being read
+                if character == "X":
+                    currently_reading = "X"
+                elif character == "Y":
+                    currently_reading = "Y"
+                elif character == "Z":
+                    currently_reading = "Z"
+
+            # Convert the string chunks to floats
+            try:
+                x_field = float(x_string)
+            except Exception as err:
+                x_field = None
+                print("Could not determine x field from string {} | {}"
+                      .format(x_string, err))
+            try:
+                y_field = float(y_string)
+            except Exception as err:
+                y_field = None
+                print("Could not determine y field from string {} | {}"
+                      .format(y_string, err))
+            try:
+                z_field = float(z_string)
+            except Exception as err:
+                z_field = None
+                print("Could not determine z field from string {} | {}"
+                      .format(z_string, err))
+
+            return x_field, y_field, z_field
+        
+        # Print 999 so that it is not confused with 0.0
+        except AttributeError:
+            return 999, 999, 999  
+
+
 if __name__ == "__main__":
-    instruments = Instruments()
-    rm.list_resources()
+    #instruments = Instruments()
+    #rm.list_resources()
+    pass
